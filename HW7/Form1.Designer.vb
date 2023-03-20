6b575690bf579cb2fc277a455ca556f4aae787cb
rcfant89 <rcfant89@ALPHAONE>
2023-03-20 13:41:24 -0400

    Add project files.

---
+++ HW7/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cboJobs = New System.Windows.Forms.ComboBox()
+        Me.lblHourlyPay = New System.Windows.Forms.Label()
+        Me.lblCurrentPayTitle = New System.Windows.Forms.Label()
+        Me.lblCurrentYearlyTitle = New System.Windows.Forms.Label()
+        Me.lblAnnualPay = New System.Windows.Forms.Label()
+        Me.lblFiveYearProjectionTitle = New System.Windows.Forms.Label()
+        Me.lblFiveYearProjectionAnnual = New System.Windows.Forms.Label()
+        Me.lblFYPHourly = New System.Windows.Forms.Label()
+        Me.lblFYPYearly = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cboJobs
+        '
+        Me.cboJobs.FormattingEnabled = True
+        Me.cboJobs.Location = New System.Drawing.Point(300, 143)
+        Me.cboJobs.Name = "cboJobs"
+        Me.cboJobs.Size = New System.Drawing.Size(370, 24)
+        Me.cboJobs.TabIndex = 1
+        '
+        'lblHourlyPay
+        '
+        Me.lblHourlyPay.AutoSize = True
+        Me.lblHourlyPay.Location = New System.Drawing.Point(486, 261)
+        Me.lblHourlyPay.Name = "lblHourlyPay"
+        Me.lblHourlyPay.Size = New System.Drawing.Size(100, 17)
+        Me.lblHourlyPay.TabIndex = 2
+        Me.lblHourlyPay.Text = "Current Hourly"
+        '
+        'lblCurrentPayTitle
+        '
+        Me.lblCurrentPayTitle.AutoSize = True
+        Me.lblCurrentPayTitle.Location = New System.Drawing.Point(297, 261)
+        Me.lblCurrentPayTitle.Name = "lblCurrentPayTitle"
+        Me.lblCurrentPayTitle.Size = New System.Drawing.Size(182, 17)
+        Me.lblCurrentPayTitle.TabIndex = 3
+        Me.lblCurrentPayTitle.Text = "Current Median Hourly Pay:"
+        '
+        'lblCurrentYearlyTitle
+        '
+        Me.lblCurrentYearlyTitle.AutoSize = True
+        Me.lblCurrentYearlyTitle.Location = New System.Drawing.Point(297, 292)
+        Me.lblCurrentYearlyTitle.Name = "lblCurrentYearlyTitle"
+        Me.lblCurrentYearlyTitle.Size = New System.Drawing.Size(181, 17)
+        Me.lblCurrentYearlyTitle.TabIndex = 4
+        Me.lblCurrentYearlyTitle.Text = "Current Median Yearly Pay:"
+        '
+        'lblAnnualPay
+        '
+        Me.lblAnnualPay.AutoSize = True
+        Me.lblAnnualPay.Location = New System.Drawing.Point(486, 292)
+        Me.lblAnnualPay.Name = "lblAnnualPay"
+        Me.lblAnnualPay.Size = New System.Drawing.Size(99, 17)
+        Me.lblAnnualPay.TabIndex = 5
+        Me.lblAnnualPay.Text = "Current Yearly"
+        '
+        'lblFiveYearProjectionTitle
+        '
+        Me.lblFiveYearProjectionTitle.AutoSize = True
+        Me.lblFiveYearProjectionTitle.Location = New System.Drawing.Point(297, 321)
+        Me.lblFiveYearProjectionTitle.Name = "lblFiveYearProjectionTitle"
+        Me.lblFiveYearProjectionTitle.Size = New System.Drawing.Size(184, 17)
+        Me.lblFiveYearProjectionTitle.TabIndex = 6
+        Me.lblFiveYearProjectionTitle.Text = "Five Year Projection Hourly:"
+        '
+        'lblFiveYearProjectionAnnual
+        '
+        Me.lblFiveYearProjectionAnnual.AutoSize = True
+        Me.lblFiveYearProjectionAnnual.Location = New System.Drawing.Point(297, 348)
+        Me.lblFiveYearProjectionAnnual.Name = "lblFiveYearProjectionAnnual"
+        Me.lblFiveYearProjectionAnnual.Size = New System.Drawing.Size(183, 17)
+        Me.lblFiveYearProjectionAnnual.TabIndex = 7
+        Me.lblFiveYearProjectionAnnual.Text = "Five Year Projection Yearly:"
+        '
+        'lblFYPHourly
+        '
+        Me.lblFYPHourly.AutoSize = True
+        Me.lblFYPHourly.Location = New System.Drawing.Point(489, 321)
+        Me.lblFYPHourly.Name = "lblFYPHourly"
+        Me.lblFYPHourly.Size = New System.Drawing.Size(79, 17)
+        Me.lblFYPHourly.TabIndex = 8
+        Me.lblFYPHourly.Text = "FYP Hourly"
+        '
+        'lblFYPYearly
+        '
+        Me.lblFYPYearly.AutoSize = True
+        Me.lblFYPYearly.Location = New System.Drawing.Point(489, 348)
+        Me.lblFYPYearly.Name = "lblFYPYearly"
+        Me.lblFYPYearly.Size = New System.Drawing.Size(78, 17)
+        Me.lblFYPYearly.TabIndex = 9
+        Me.lblFYPYearly.Text = "FYP Yearly"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(489, 395)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 17)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(957, 629)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblFYPYearly)
+        Me.Controls.Add(Me.lblFYPHourly)
+        Me.Controls.Add(Me.lblFiveYearProjectionAnnual)
+        Me.Controls.Add(Me.lblFiveYearProjectionTitle)
+        Me.Controls.Add(Me.lblAnnualPay)
+        Me.Controls.Add(Me.lblCurrentYearlyTitle)
+        Me.Controls.Add(Me.lblCurrentPayTitle)
+        Me.Controls.Add(Me.lblHourlyPay)
+        Me.Controls.Add(Me.cboJobs)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cboJobs As ComboBox
+    Friend WithEvents lblHourlyPay As Label
+    Friend WithEvents lblCurrentPayTitle As Label
+    Friend WithEvents lblCurrentYearlyTitle As Label
+    Friend WithEvents lblAnnualPay As Label
+    Friend WithEvents lblFiveYearProjectionTitle As Label
+    Friend WithEvents lblFiveYearProjectionAnnual As Label
+    Friend WithEvents lblFYPHourly As Label
+    Friend WithEvents lblFYPYearly As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ HW7/Form1.vb
@@ -0,0 +1,85 @@
+﻿' Program Name: HW7
+' Author:       Rod Fant
+' Date:         3/20/2023
+' Purpose:      HW7 provides a list of four careers. A user can select each of the jobs and
+'               will be shown the current hourly and annual pay, as well as the projected pay
+'               based on a 4% raise for 5 years in a row.
+
+Option Strict On
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Put "Select a Job" text shown and add jobs to the box on load.
+        cboJobs.Items.Add("Select a Job")
+        cboJobs.Items.Add("Computer Developer")
+        cboJobs.Items.Add("Computer System Analyst")
+        cboJobs.Items.Add("Computer and Information Systems Manager")
+        cboJobs.Items.Add("Computer Software Engineer, Applications")
+        cboJobs.SelectedIndex = 0 ' Select the default "Select a Job" text
+    End Sub
+
+    Private Sub cboJobs_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboJobs.SelectedIndexChanged
+        ' Initializes and sets the four variables to 0
+        Dim hourlyPay As Double = 0
+        Dim annualPay As Double = 0
+        Dim projHourlyPay As Double = 0
+        Dim projAnnualPay As Double = 0
+        GetCurrentAndProjectedPay(cboJobs.SelectedIndex, hourlyPay, annualPay, projHourlyPay, projAnnualPay)
+        ' Displays the results in these labels below. C2 formats to currency
+        lblHourlyPay.Text = hourlyPay.ToString("C2")
+        lblAnnualPay.Text = annualPay.ToString("C2")
+        lblFYPHourly.Text = projHourlyPay.ToString("C2")
+        lblFYPYearly.Text = projAnnualPay.ToString("C2")
+    End Sub
+
+    Private Function ProjectedCalculations(ByRef payRate As Double) As Double
+        ' This function accepts the data in a new/unused variable "payRate" (because instructions said "You cannot use variables declared outside the function)
+        ' Projection is a new variable found nowhere else
+        Dim projection As Double = payRate
+        ' Projection is calculated (prob. should us a loop)
+        projection = payRate * 1.04 * 1.04 * 1.04 * 1.04 * 1.04
+
+        ' Projection is returned whenever called
+        Return projection
+
+    End Function
+
+
+
+    Private Sub GetCurrentAndProjectedPay(ByVal occupationIndex As Integer, ByRef hourlyPay As Double, ByRef annualPay As Double, ByRef projHourlyPay As Double, ByRef projAnnualPay As Double)
+        ' The function "GetCurrentAndProjectedPay" accepts the selected job, and the four pay variables
+        ' Next we declare the pay for each
+        Dim computerDeveloperHourlyMean As Double = 46.46
+        Dim computerDeveloperAnnualMean As Double = 96650
+        Dim computerSystemsAnalystsHourlyMean As Double = 49.14
+        Dim computerSystemsAnalystsAnnualMean As Double = 102210
+        Dim systemsManagerHourlyMean As Double = 43.87
+        Dim systemsManagerAnnualMean As Double = 91250
+        Dim computerSoftwareEngineersHourlyMean As Double = 58.17
+        Dim computerSoftwareEngineersAnnualMean As Double = 120990
+        ' The integer (selected job from the combo box) outputs the corresponding pay rates
+        Select Case occupationIndex
+            Case 1 'Computer Developer
+                hourlyPay = computerDeveloperHourlyMean ' Displays the given data
+                annualPay = computerDeveloperAnnualMean ' Displays the given data
+                projHourlyPay = ProjectedCalculations(hourlyPay) ' Calls the function "ProjectedCalculations" and passes the data to be calculated
+                projAnnualPay = ProjectedCalculations(annualPay) ' Calls the function "ProjectedCalculations" and passes the data to be calculated
+            Case 2 'Computer Systems Analysts
+                hourlyPay = computerSystemsAnalystsHourlyMean ' Same as above block
+                annualPay = computerSystemsAnalystsAnnualMean
+                projHourlyPay = ProjectedCalculations(hourlyPay)
+                projAnnualPay = ProjectedCalculations(annualPay)
+            Case 3 'Systems Manager
+                hourlyPay = systemsManagerHourlyMean ' Same as above block
+                annualPay = systemsManagerAnnualMean
+                projHourlyPay = ProjectedCalculations(hourlyPay)
+                projAnnualPay = ProjectedCalculations(annualPay)
+            Case 4 'Computer Software Engineers
+                hourlyPay = computerSoftwareEngineersHourlyMean ' Same as above block
+                annualPay = computerSoftwareEngineersAnnualMean
+                projHourlyPay = ProjectedCalculations(hourlyPay)
+                projAnnualPay = ProjectedCalculations(annualPay)
+        End Select
+
+    End Sub
+End Class
